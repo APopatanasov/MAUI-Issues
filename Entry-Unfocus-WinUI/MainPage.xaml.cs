b3d4bd6bdcb27101b75ea7ee543993249eb04004
Atanas Popatanasov <atanas.popatanasov@progress.com>
2022-10-03 15:32:47 +0300

    Add Entry focus issue.

---
+++ Entry-Unfocus-WinUI/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace HelloMaui;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        this.Navigation.PushAsync(new NavPage());
+    }
+
+    private void entry_Focused(object sender, FocusEventArgs e)
+    {
+        this.log.Text += "Focused" + Environment.NewLine;
+    }
+
+    private void entry_Unfocused(object sender, FocusEventArgs e)
+    {
+        this.log.Text += "Unfocused" + Environment.NewLine;
+    }
+
+    private void entry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (this.entry.Text.ToLower() == "unfocus")
+        {
+            this.entry.Unfocus();
+        }
+    }
+
+    class NavPage : ContentPage
+    {
+        public NavPage()
+        {
+            var lb = new Label();
+            lb.HorizontalTextAlignment = Microsoft.Maui.TextAlignment.Center;
+            lb.VerticalTextAlignment = Microsoft.Maui.TextAlignment.Center;
+            lb.FontSize = 18;
+            lb.Text = "Empty Page. Navigate back.";
+
+            this.Content = lb;
+        }
+    }
+}
